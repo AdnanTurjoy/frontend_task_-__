52db26a25a5eeb4003218b5a493a7631f8babc20
adel.adnan <adel.adnan@businessaccelerate.com.bd>
2024-01-18 12:54:35 +0600

    TS Fix

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,243 +1,229 @@
-import React, { useEffect, useRef, useState } from 'react';
-
-import type { GetRef, PaginationProps, TableColumnsType, TableColumnType } from 'antd';
-import { Avatar, Card, Dropdown, Form, Input, Pagination, Space, Table, Typography } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-import { useDeleteUserMutation, useEditUserMutation, useGetSingleUserMutation, useGetUsersByPageMutation, useGetUsersQuery } from '../features/apiSlice';
-import type { MenuProps } from 'antd';
-import { Button, Modal } from 'antd';
-import Meta from 'antd/es/card/Meta';
-import { ExclamationCircleFilled } from '@ant-design/icons';
+import React, { useEffect, useState } from 'react';
+import { Modal, Form, Input, Button, Table, Space, Pagination, Card, Avatar, Typography, PaginationProps } from 'antd';
+import { ExclamationCircleFilled, UserOutlined } from '@ant-design/icons';
+import { GetRef } from 'antd/lib/input';
+import {
+  useDeleteUserMutation,
+  useEditUserMutation,
+  useGetSingleUserMutation,
+  useGetUsersByPageMutation,
+  useGetUsersQuery,
+} from '../features/apiSlice';
 import CreateForm from '../components/CreateForm';
+
+const { confirm } = Modal;
+const { Meta } = Card;
+const { Text } = Typography;
+
+interface DataType {
+  id: number;
+  email: string;
+  avatar: string;
+  first_name: string;
+  last_name: string;
+}
 
 type InputRef = GetRef<typeof Input>;
 
-interface DataType {
-	id: Number;
-	email: string;
-	avatar: string;
-	first_name: string;
-	last_name: string;
-}
-
 const Dashboard: React.FC = () => {
-	const { confirm } = Modal;
-	
-	const [deleteUser, { isLoading: deleteLoading }] = useDeleteUserMutation()
-	const [editUser, { isLoading: editLoading }] = useEditUserMutation()
-	const [viewUser, { isLoading: viewLoading }] = useGetSingleUserMutation()
-	const [getUsers, { isLoading: userLoading }] = useGetUsersByPageMutation()
-	const [isModalOpen, setIsModalOpen] = useState({
-		open: false,
-		type: ""
-	});
-	const [formValues, setFormValues] = useState({ name: "", job: "" })
-	const [user, setUser] = useState<DataType>({})
-	const [dataSource, setDataSource] = useState([]);
-	const [totalPages, setTotalPages] = useState(1);
-	const [loading, setLoading] = useState(false);
+  const [deleteUser, { isLoading: deleteLoading }] = useDeleteUserMutation();
+  const [editUser, { isLoading: editLoading }] = useEditUserMutation();
+  const [viewUser, { isLoading: viewLoading }] = useGetSingleUserMutation();
+  const [getUsers, { isLoading: userLoading }] = useGetUsersByPageMutation();
+  const [isModalOpen, setIsModalOpen] = useState<{ open: boolean; type: string }>({
+    open: false,
+    type: '',
+  });
+  const [formValues, setFormValues] = useState<{ name: string; job: string }>({ name: '', job: '' });
+  const [user, setUser] = useState<DataType>({});
+  const [dataSource, setDataSource] = useState<DataType[]>([]);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(false);
 
-	useEffect(() => {
-		fetchRecords(1);
-	  }, []);
+  useEffect(() => {
+    fetchRecords(1);
+  }, []);
 
-	  const fetchRecords = async (page) => {
-		setLoading(true);
-		try {
-			const users = await getUsers(page)
-			console.log(users.data.data)
-		    setDataSource(users?.data)
-			setTotalPages(res.data.total_pages);
-			setLoading(false);
-		} catch (error) {
-			setLoading(false);
-		}
-		
-	  };
-	const handleOk = async () => {
-		if (isModalOpen.type === "_EDIT") {
-			try {
-				const response = await editUser({ ...formValues, id: user.id }).unwrap()
-				console.log(response);
-				setIsModalOpen({ open: false, type: "" });
-				setFormValues({ name: "", job: "" })
+  const fetchRecords = async (page: number) => {
+    setLoading(true);
+    try {
+      const users = await getUsers(page);
+      setDataSource(users?.data);
+      setTotalPages(users?.data.total_pages);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
 
-			} catch (error) {
-				console.log(error);
-			}
-		} else {
-			setIsModalOpen({ open: false, type: "" });
-		}
+  const handleOk = async () => {
+    if (isModalOpen.type === '_EDIT') {
+      try {
+        const response = await editUser({ ...formValues, id: user.id }).unwrap();
+        console.log(response);
+        setIsModalOpen({ open: false, type: '' });
+        setFormValues({ name: '', job: '' });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      setIsModalOpen({ open: false, type: '' });
+    }
+  };
 
+  const handleCancel = () => {
+    setUser({});
+    setFormValues({ name: '', job: '' });
+    setIsModalOpen({ open: false, type: '' });
+  };
 
-	};
+  const columns = [
+    {
+      title: 'Avatar',
+      dataIndex: 'avatar',
+      key: 'avatar',
+      width: '20%',
+      render: (_, { avatar }) => (
+        <>
+          <img style={{ borderRadius: '50%', height: '45px', width: '45px' }} src={avatar} alt="" />
+        </>
+      ),
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+      width: '30%',
+    },
+    {
+      title: 'First Name',
+      dataIndex: 'first_name',
+      key: 'first_name',
+      width: '20%',
+    },
+    {
+      title: 'Last Name',
+      dataIndex: 'last_name',
+      key: 'last_name',
+      width: '20%',
+    },
+    {
+      title: 'Action',
+      dataIndex: 'actions',
+      key: 'actions',
+      width: '30%',
+      render: (_: any, record: DataType) => (
+        <Space size="middle">
+          <a onClick={() => onSingleUser(record.id, '_VIEW')} style={{ marginRight: 8 }}>
+            View
+          </a>
+          <a onClick={() => onSingleUser(record.id, '_EDIT')} style={{ marginRight: 8 }}>
+            Edit
+          </a>
+          <a onClick={() => showDeleteConfirm(record.id)} style={{ marginRight: 8 }}>
+            Delete
+          </a>
+        </Space>
+      ),
+    },
+  ];
 
-	const handleCancel = () => {
-		setUser({})
-		setFormValues({})
-		setIsModalOpen({ open: false, type: "" });
-	};
+  const handleEditForm = (event: { target: { name: string; value: string } }) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-	const columns: TableColumnsType<DataType> = [
-		{
-			title: 'Avatar',
-			dataIndex: 'avatar',
-			key: 'avatar',
-			width: '20%',
-			render: (_, { avatar }) => (
-				<>
-					<img style={{ borderRadius: "50%", height: "45px", width: "45px" }} src={avatar} alt="" />
+  const showDeleteConfirm = (id: number) => {
+    confirm({
+      title: 'Are you sure delete this task?',
+      icon: <ExclamationCircleFilled />,
+      content: 'Some descriptions',
+      okText: 'Yes',
+      okType: 'danger',
+      cancelText: 'No',
+      onOk() {
+        onDelete(id);
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  };
 
-				</>
-			),
+  const onDelete = async (id: number) => {
+    try {
+      const response = await deleteUser(id).unwrap();
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-		},
-		{
-			title: 'Email',
-			dataIndex: 'email',
-			key: 'email',
-			width: '30%',
+  const onSingleUser = async (id: number, type: string) => {
+    try {
+      const response = await viewUser(id).unwrap();
+      setUser(response.data);
+      setIsModalOpen({ open: true, type });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-		},
-		{
-			title: 'First Name',
-			dataIndex: 'first_name',
-			key: 'first_name',
-			width: '20%',
+  const handleCreate = () => {
+    setIsModalOpen({ open: true, type: '_CREATE' });
+  };
 
-		},
-		{
-			title: 'Last Name',
-			dataIndex: 'last_name',
-			key: 'last_name',
-			width: '20%',
+  const onPageChange: PaginationProps['onChange'] = (page) => {
+    fetchRecords(page);
+  };
 
-		},
-		{
-			title: 'Action',
-			dataIndex: 'actions',
-			key: 'actions',
-			width: '30%',
-			render: (_: any, record: DataType) => (
-				<Space size="middle">
-					<a onClick={() => onSingleUser(record.id, "_VIEW")} style={{ marginRight: 8 }}>
-						View
-					</a>
-					<a onClick={() => onSingleUser(record.id, "_EDIT")} style={{ marginRight: 8 }}>
-						Edit
-					</a>
+  return (
+    <>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" onClick={handleCreate}>
+          Create
+        </Button>
+      </Form.Item>
 
-					<a onClick={() => showDeleteConfirm(record.id)} style={{ marginRight: 8 }}>
-						Delete
-					</a>
+      <Table loading={loading} columns={columns} dataSource={dataSource?.data} pagination={false}></Table>
+      <Pagination
+        style={{ marginTop: '10px' }}
+        current={dataSource?.page}
+        onChange={onPageChange}
+        total={50}
+      />
 
-				</Space>
-			),
-		},
-	];
-	const handleEditForm = (event: { target: { name: string; value: string; }; }) => {
-		const { name, value } = event.target;
-		setFormValues({ ...formValues, [name]: value });
-	}
-	const showDeleteConfirm = (id: Number) => {
-		confirm({
-			title: 'Are you sure delete this task?',
-			icon: <ExclamationCircleFilled />,
-			content: 'Some descriptions',
-			okText: 'Yes',
-			okType: 'danger',
-			cancelText: 'No',
-			onOk() {
-				onDelete(id);
-			},
-			onCancel() {
-				console.log('Cancel');
-			},
-		});
-	};
-	const onDelete = async (id: Number) => {
-		try {
-			const response = await deleteUser(id).unwrap()
-			console.log(response);
-
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	const onSingleUser = async (id: Number, type: string) => {
-		try {
-			const response = await viewUser(id).unwrap()
-			setUser(response.data)
-			setIsModalOpen({ open: true, type });
-
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	const handleCreate = () => {
-		setIsModalOpen({ open: true, type: "_CREATE" });
-	}
-	const onPageChange: PaginationProps['onChange'] = (page) => {
-		fetchRecords(page);
-	  };
-	
-
-	return <>
-		<Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-			<Button type="primary" onClick={handleCreate}>
-				Create
-			</Button>
-		</Form.Item>
-		
-			  <Table
-			  loading={loading}
-			  columns={columns}
-			  dataSource={dataSource.data}
-			  pagination={false}
-			></Table>
-			<Pagination style={{marginTop:"10px"}} current={dataSource.page} onChange={onPageChange} total={50} />
-		
-		<Modal title="User" open={isModalOpen.open} onOk={handleOk} onCancel={handleCancel}
-			cancelButtonProps={{ style: { display: 'none' } }}
-			okButtonProps={{
-				style: isModalOpen.type !== "_EDIT" ? {
-					display: 'none'
-				} : {}
-			}}
-		>
-			<Card
-				style={{ width: 450, }}
-				cover={
-					isModalOpen.type !== "_CREATE" && <img
-						alt="avatar"
-						src={user.avatar}
-					/>
-				}
-
-			>
-				{
-					isModalOpen.type === "_EDIT" ?
-						<>
-
-							<input name="name" type="text" placeholder='Your new name' onChange={handleEditForm} />
-							<input name="job" type="text" placeholder='Your Job' onChange={handleEditForm} />
-						</>
-						: isModalOpen.type === "_VIEW" ?
-							<Meta
-								avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
-								title={user.first_name}
-								description={user.email}
-							>
-
-							</Meta> : <CreateForm />
-				}
-
-
-			</Card>
-		</Modal>
-	</>;
+      <Modal
+        title="User"
+        visible={isModalOpen.open}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        cancelButtonProps={{ style: { display: 'none' } }}
+        okButtonProps={{
+          style: isModalOpen.type !== '_EDIT' ? {
+            display: 'none',
+          } : {},
+        }}
+      >
+        <Card style={{ width: 450 }} cover={isModalOpen.type !== '_CREATE' && <img alt="avatar" src={user.avatar} />}>
+          {isModalOpen.type === '_EDIT' ? (
+            <>
+              <input name="name" type="text" placeholder="Your new name" onChange={handleEditForm} />
+              <input name="job" type="text" placeholder="Your Job" onChange={handleEditForm} />
+            </>
+          ) : isModalOpen.type === '_VIEW' ? (
+            <Meta
+              avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
+              title={user.first_name}
+              description={user.email}
+            />
+          ) : (
+            <CreateForm />
+          )}
+        </Card>
+      </Modal>
+    </>
+  );
 };
 
 export default Dashboard;
